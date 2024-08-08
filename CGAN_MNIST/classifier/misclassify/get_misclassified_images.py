1980e359a6eeb3e4c4b838c10161fc3e786a4613
LiangXin1001 <XinLiang0920@gmail.com>
2024-08-08 15:39:49 -0400

    relative path

--- CGAN_MNIST/classifier/misclassify/get_misclassified_images.py
+++ CGAN_MNIST/classifier/misclassify/get_misclassified_images.py
@@ -18,16 +18,12 @@ import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
-from PIL import Image
-import numpy as np
-import pandas as pd
 import os
+import pandas as pd
+import numpy as np
+from PIL import Image
 
-
- 
- 
-
-def get_mean_std(csv_file, image_dirs):
+def get_mean_std(csv_file, image_dir):
     # 读取CSV文件
     df = pd.read_csv(csv_file)
 
@@ -38,34 +34,31 @@ def get_mean_std(csv_file, image_dirs):
 
     # 遍历图像路径，计算均值和方差
     for image_name in df['image_name']:
-        image_found = False
-        for image_dir in image_dirs:
-            image_path = os.path.join(image_dir, image_name)
-            if os.path.exists(image_path):
-                with Image.open(image_path) as img:
-                    img = img.convert('RGB')
-                    img_array = np.array(img) / 255.0  # 转换为0-1范围
-                    sum_rgb += img_array.sum(axis=(0, 1))  # 对所有像素进行加和
-                    sum_squares_rgb += (img_array**2).sum(axis=(0, 1))  # 对所有像素的平方进行加和
-                image_count += img_array.shape[0] * img_array.shape[1]  # 总像素数
-                image_found = True
-                break
-        if not image_found:
-            print(f"Image {image_name} not found in any of the directories.")
+        image_path = os.path.join(image_dir, image_name)
+        if os.path.exists(image_path):
+            with Image.open(image_path) as img:
+                img = img.convert('RGB')
+                img_array = np.array(img) / 255.0  # 转换为0-1范围
+                sum_rgb += img_array.sum(axis=(0, 1))  # 对所有像素进行加和
+                sum_squares_rgb += (img_array**2).sum(axis=(0, 1))  # 对所有像素的平方进行加和
+            image_count += img_array.shape[0] * img_array.shape[1]  # 总像素数
+        else:
+            print(f"Image {image_name} not found in the directory.")
 
     if image_count == 0:
-        raise ValueError("No images found in the specified directories.")
+        raise ValueError("No images found in the specified directory.")
 
     # 计算全局均值
     mean_rgb = sum_rgb / image_count
 
     # 计算全局标准差
     std_rgb = np.sqrt(sum_squares_rgb / image_count - mean_rgb**2)
-    mean_rgb = np.array(mean_rgb)
-    std_rgb = np.array(std_rgb)
+
     print(f"Mean: {mean_rgb}")
     print(f"Std: {std_rgb}")
+    
     return mean_rgb, std_rgb
+
 
 
 # Set up command line arguments
